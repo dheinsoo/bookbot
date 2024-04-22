61495422979b82877fa317f282f039f97b4cdfb3
dheinsoo <dheinsoo@gmail.com>
2024-04-22 21:55:43 +0200

    second draft

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     print(f"{num_words} words found in the document")
+    letters = get_num_letters(text)
+    print(f'These letters are used:')
+    print(letters)
 
 
 def get_num_words(text):
@@ -14,7 +17,34 @@ def get_book_text(path):
     with open(path) as f:
         return f.read()
     
+def get_num_letters(text):
+    letter_dict = {}
+    for letter in text:
+        if letter.isalpha() == False:
+            continue
+        if letter.lower() not in letter_dict:
+            letter_dict[letter.lower()] = 1
+        else:
+            letter_dict[letter.lower()] += 1
 
-    
+    return letter_dict
+
+def convert_dict(dict):
+    list = []
+    for key in dict.keys():
+        new_dict = {}
+        new_dict["char"] = key
+        new_dict["count"] = dict[key]
+        list.append(new_dict)
+        
+    return new_dict
+
+
+
+
+def sort_occurences(dict):
+    return dict["count"]
+
+
 if __name__== '__main__':
     main()
